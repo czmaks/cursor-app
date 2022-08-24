453cad5ac76cebafb2bcdd03b76915be47049a73
maks <maks@makss-MacBook-Pro.local>
2022-08-24 18:21:18 +0200

    custom cursor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { motion } from 'framer-motion';
 
 function App() {
+
+  const [cursorPos, setCursorPos] = useState({
+    x: window.innerWidth/2,
+    y: window.innerHeight/2
+  });
+
+  const [variant, setVariant] = useState('default');
+
+  console.log(cursorPos);
+
+  useEffect(() => {
+    const mouseMove = e => {
+        setCursorPos({
+          x: e.clientX,
+          y: e.clientY
+        });
+    }
+    window.addEventListener('mousemove', mouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMove);
+    }
+  },[]);
+
+  
+  const variants = {
+    default: {
+      x: cursorPos.x - window.innerWidth/2,
+      y: cursorPos.y - window.innerHeight/2
+    },
+    text: {
+      x: cursorPos.x - window.innerWidth/2,
+      y: cursorPos.y - window.innerHeight/2,
+      backgroundColor: 'yellow',
+      height: 150,
+      width: 150,
+      mixBlendMode: 'difference'
+    }
+  }
+  
+  const textEnter = () => setVariant('text');
+  const textLeave = () => setVariant('default');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 onMouseEnter={textEnter} onMouseLeave={textLeave} className="header">Hello World</h1>
+      <motion.div
+        className="cursor"
+        variants={variants}
+        animate={variant}
+       />
     </div>
   );
 }
